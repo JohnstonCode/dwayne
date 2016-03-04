927a09df0f2066c5174598b73249fe47bbf272ed
Christopher <chrisjjcode@gmail.com>
2016-03-04 19:33:39 +0000

    now displays number of votes

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+
+include('connect.php');
+
+$query = 'SELECT * FROM votes';
+
+$result = $conn->query($query);
+
+$votes = [];
+
+while($row = $result->fetch_array(MYSQLI_ASSOC))
+{
+    $votes[] = $row;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="">
     <head>
@@ -40,7 +56,19 @@
             </nav>
             <div class="jumbotron row" style="text-align: center;">
                 <h1>Is Dwayne insane?</h1>
-                <img id="vote-image" src="images/dwayne-01.jpg"></img><br />
+                <div class="panel panel-default col-md-3 insane-counter col-md-offset-1 ">
+                  <div class="panel-body">
+                    <?php echo $votes[0]['number_of_votes']; ?>
+                  </div>
+                  <div class="panel-footer">Insane Votes</div>
+                </div>
+                <img class="col-md-4" id="vote-image" src="images/dwayne-01.jpg"></img><br />
+                <div class="panel panel-default col-md-3 sane-counter">
+                  <div class="panel-body">
+                    <?php echo $votes[1]['number_of_votes']; ?>
+                  </div>
+                  <div class="panel-footer">Sane Votes</div>
+                </div>
                 <div class="panel panel-primary col-md-6 col-md-offset-3" id="main-vote">
                     <div class="panel-heading col-md-12">
                         <h3 class="panel-title">Vote Now!</h3>
