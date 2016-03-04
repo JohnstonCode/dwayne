2cf6d81b214a9aa4f0083986035c2720e52c4db5
Christopher <chrisjjcode@gmail.com>
2016-03-04 14:19:31 +0000

    added basic structure to main page

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/normalize.css">
+        <link href="https://bootswatch.com/paper/bootstrap.css" rel="stylesheet">
         <link rel="stylesheet" href="css/main.css">
 
     </head>
@@ -16,9 +16,42 @@
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-
-        <!-- Add your site or application content here -->
-        
+        <div id="page-wrapper">
+            <nav class="navbar navbar-default">
+              <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Dwayne</a>
+                </div>
+            
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
+            <div class="jumbotron row" style="text-align: center;">
+                <h1>Is Dwayne insane?</h1>
+                <img id="vote-image" src="images/dwayne-01.jpg"></img><br />
+                <div class="panel panel-primary col-md-6 col-md-offset-3" id="main-vote">
+                    <div class="panel-heading col-md-12">
+                        <h3 class="panel-title">Vote Now!</h3>
+                    </div>
+                    <div class="panel-body" id="vote-buttons">
+                        <button type="button" class="btn btn-danger">Insane</button>
+                        <button type="button" class="btn btn-success">Sane</button>  
+                    </div>
+                </div>
+            </div>
+        </div><!-- END of page wrapper -->
         
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
